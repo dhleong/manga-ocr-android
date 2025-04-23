82a74c6695ba94c74ae0592f5d6848b63d92f081
Daniel Leong <me@dhleong.net>
2025-04-23 10:28:36 -0400

    Use the original colors---much more accurate!

--- library/src/main/java/net/dhleong/mangaocr/ImageProcessor.kt
+++ library/src/main/java/net/dhleong/mangaocr/ImageProcessor.kt
@@ -13,10 +13,6 @@ class ImageProcessor<T>(
     private val inputHeight = 224
     private val inputWidth = 224
 
-    // Normalization parameters from ViTImageProcessor
-    private val mean = floatArrayOf(0.485f, 0.456f, 0.406f)
-    private val std = floatArrayOf(0.229f, 0.224f, 0.225f)
-
     // Allocate buffer for tensor
     private val buffer = FloatBuffer.allocate(3 * inputHeight * inputWidth)
     private val shape = longArrayOf(1, 3, inputHeight.toLong(), inputWidth.toLong())
@@ -35,24 +31,17 @@ class ImageProcessor<T>(
             for (x in 0 until inputWidth) {
                 val pixel = resizedBitmap.getPixel(x, y)
 
+                // NOTE: This *works* but seems less accurate than the original colors?
+//                // in [0, 1]
+//                val gray = Color.luminance(pixel) * 255f
+//                val r = gray
+//                val g = gray
+//                val b = gray
+
                 // in [0, 1]
-                val gray = (0xffFFFFFF * Color.luminance(pixel)).toInt()
-                val r = Color.red(gray)
-                val g = Color.green(gray)
-                val b = Color.blue(gray)
-
-                // normalize to [-1, 1]
-                val normalized = (gray - 0.5f) / 0.5f
-
-//                // Extract RGB values
-//                val r = Color.red(pixel) / 255.0f
-//                val g = Color.green(pixel) / 255.0f
-//                val b = Color.blue(pixel) / 255.0f
-//
-//                // Normalize and store in CHW order (PyTorch format)
-//                buffer.put((r - mean[0]) / std[0])
-//                buffer.put((g - mean[1]) / std[1])
-//                buffer.put((b - mean[2]) / std[2])
+                val r = Color.red(pixel)
+                val g = Color.green(pixel)
+                val b = Color.blue(pixel)
 
                 val ri = y * inputWidth + x
                 val gi = 1 * inputWidth * inputHeight + ri
@@ -60,14 +49,8 @@ class ImageProcessor<T>(
                 buffer.put(ri, (r / 255f - 0.5f) / 0.5f)
                 buffer.put(gi, (g / 255f - 0.5f) / 0.5f)
                 buffer.put(bi, (b / 255f - 0.5f) / 0.5f)
-
-//                buffer.put(normalized)
-//                buffer.put(normalized)
-//                buffer.put(normalized)
             }
         }
-
-//        buffer.flip()
 
         if (resizedBitmap !== bitmap) {
             resizedBitmap.recycle()
