27568748acd868dc954a103aa9d3258afd39baec
Daniel Leong <me@dhleong.net>
2025-04-27 16:55:01 -0400

    Attempt to improve IOException handling in manager init

--- library/src/main/java/net/dhleong/mangaocr/BaseManager.kt
+++ library/src/main/java/net/dhleong/mangaocr/BaseManager.kt
@@ -1,6 +1,7 @@
 package net.dhleong.mangaocr
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.AtomicReference
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleCoroutineScope
@@ -8,21 +9,34 @@ import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 abstract class BaseManager<T>(
     context: Context,
     scope: LifecycleCoroutineScope,
     private val lifecycle: Lifecycle,
 ) {
-    private val model: AtomicReference<T?> = AtomicReference(null)
-    private val modelLoaded = MutableSharedFlow<Unit>()
+    sealed interface State<out T> {
+        data object Error : State<Nothing>
+
+        data object Loading : State<Nothing>
+
+        data class Loaded<T>(
+            val model: T,
+        ) : State<T>
+    }
+
+    private val context = context.applicationContext
+    private val model: AtomicReference<State<T>?> = AtomicReference(null)
+    private val modelLoaded = MutableSharedFlow<T>()
 
     init {
         scope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                if (model.get() == null) {
-                    model.set(initialize(context))
-                    modelLoaded.emit(Unit)
+                try {
+                    maybeInit()
+                } catch (e: IOException) {
+                    return@repeatOnLifecycle
                 }
             }
         }
@@ -30,13 +44,24 @@ abstract class BaseManager<T>(
 
     protected abstract suspend fun initialize(context: Context): T
 
-    protected suspend fun awaitModel(): T {
-        val existing = model.get()
-        if (existing != null) {
-            return existing
-        }
+    protected suspend fun awaitModel(): T = maybeInit()
 
-        modelLoaded.first()
-        return requireNotNull(model.get())
-    }
+    private suspend fun maybeInit(): T =
+        when (val existing = model.getAndUpdate { it ?: State.Loading }) {
+            null, is State.Error -> {
+                try {
+                    val newModel = initialize(context)
+                    model.set(State.Loaded(newModel))
+                    modelLoaded.emit(newModel)
+                    newModel
+                } catch (e: IOException) {
+                    // Likely had to download something and couldn't
+                    Log.w("BaseManager", "Failed to initialize $this", e)
+                    model.set(State.Error)
+                    throw e
+                }
+            }
+            is State.Loading -> modelLoaded.first()
+            is State.Loaded -> existing.model
+        }
 }
