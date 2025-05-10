7aaf3e63d660aa0109c8d521feea36dc1560a51c
Daniel Leong <me@dhleong.net>
2025-05-09 21:33:50 -0400

    Fix: convert the decoder *only* to tflite!

--- model-dev/convert/mangaocr.py
+++ model-dev/convert/mangaocr.py
@@ -16,20 +16,12 @@ def convert_to_pt2():
     torch.export.save(exported, OUTPUTS / "manga-ocr.pt2")
 
 
-def convert():
-    model = VisionEncoderDecoderModel.from_pretrained("kha-white/manga-ocr-base")
-
+def _convert_encoder(model: VisionEncoderDecoderModel | PreTrainedModel):
     encoder = cast(PreTrainedModel, model.encoder).eval()
-    decoder = cast(PreTrainedModel, model.decoder).eval()
     print("encoder=", encoder.config, encoder)
-    print("decoder=", decoder.config, decoder)
-    print(model)
 
     quant_config = quant_recipes.full_int8_dynamic_recipe()
 
-    # decode_input_tensor = torch.zeros((1, 768), dtype=torch.float32)
-
-    print("Converting...")
     image_tensor = torch.randn((1, 3, 224, 224), dtype=torch.float32)
     edge_model = (
         ai_edge_torch.signature("encode", encoder, (image_tensor,))
@@ -37,8 +29,50 @@ def convert():
         .convert(quant_config=quant_config)
     )
 
+    output = OUTPUTS / "manga-ocr.converted.encoder.tflite"
     print(edge_model)
-    edge_model.export(str(OUTPUTS / "manga-ocr.converted.encoder.tflite"))
+    edge_model.export(str(output))
+    print(output, "->", output.stat().st_size)
+    return output
+
+
+def _convert_decoder(model: VisionEncoderDecoderModel | PreTrainedModel):
+    decoder = cast(PreTrainedModel, model.decoder).eval()
+    print("decoder=", decoder.config, decoder)
+    print(model)
+
+    quant_config = quant_recipes.full_int8_dynamic_recipe()
+
+    encoder_hidden_states = torch.randn((1, 1, 768), dtype=torch.float32)
+    input_token_ids = torch.tensor([[2]], dtype=torch.float32)
+    edge_model = (
+        ai_edge_torch.signature(
+            "decode",
+            decoder,
+            sample_kwargs={
+                "input_ids": input_token_ids,
+                "encoder_hidden_states": encoder_hidden_states,
+            },
+        )
+        # .signature("decode", decoder, decoder.dummy_inputs.values())
+        .convert(quant_config=quant_config)
+    )
+
+    output = OUTPUTS / "manga-ocr.converted.decoder.tflite"
+    print(edge_model)
+    edge_model.export(str(output))
+    print(output, "->", output.stat().st_size)
+    return output
+
+
+def convert():
+    model = VisionEncoderDecoderModel.from_pretrained("kha-white/manga-ocr-base")
+
+    # decode_input_tensor = torch.zeros((1, 768), dtype=torch.float32)
+
+    print("Converting...")
+    _convert_encoder(model)
+    _convert_decoder(model)
 
     # # Dummy input for the model
     # dummy_image = torch.randn(1, 3, 224, 224)
