b6c3a742190cbf160f437b195a331d8b0436f267
Daniel Leong <me@dhleong.net>
2026-09-16 23:01:44 -0400

    Restore `convert ogkalu-yolo` command

--- model-dev/convert/__init__.py
+++ model-dev/convert/__init__.py
@@ -1,5 +1,7 @@
 import click
 
+import download
+
 
 @click.group()
 def convert(): ...
@@ -28,3 +30,41 @@ def manga_ocr():
     from convert.mangaocr import convert
 
     convert()
+
+
+@convert.command()
+@click.option("--with-data", is_flag=True, default=False)
+def ogkalu_yolo(with_data: bool):
+    from train import dataset
+
+    dataset = dataset.build_yolo_dataset() if with_data else None
+    yolov8 = download.hf("ogkalu/manga-text-detector-yolov8s", "manga-text-detector.pt")
+
+    import ultralytics
+
+    model = ultralytics.YOLO(str(yolov8))
+    # exported = model.export(format="onnx")
+    # assert exported, "Failed to convert to onnx"
+    # processed = ops.preprocess(Path(exported))
+    # ops.dynamic(processed)
+
+    export_modes = [
+        # dict(),
+        # dict(half=True),
+        dict(int8=True),
+    ]
+
+    outputs = []
+    for mode in export_modes:
+        exported_path = model.export(
+            format="tflite", nms=True, data=str(dataset), **mode
+        )
+        output_path = OUTPUTS / Path(exported_path).name
+        if with_data:
+            output_path = output_path.with_suffix(".with_data.tflite")
+
+        shutil.move(exported_path, output_path)
+        outputs.append((mode, output_path))
+
+    for mode, output_path in outputs:
+        print(f"Output {mode} tflite_model to: ", output_path)
